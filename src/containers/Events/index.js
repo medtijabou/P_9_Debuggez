df8bd778d2c1ac350c8716154a32f7ab686cdb37
medtijabou <tijarim7@gmail.com>
2025-04-24 09:47:06 +0200

    filtrage de catégorie

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -15,9 +15,9 @@ const EventList = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const filteredEvents = (
     (!type
-      ? data?.events
-      : data?.events) || []
-  ).filter((event, index) => {
+      ?data?.events
+      :data?.events.filter((event) => event.type === type)) || []
+  ).filter((_, index) => {
     if (
       (currentPage - 1) * PER_PAGE <= index &&
       PER_PAGE * currentPage > index
