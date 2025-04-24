c6b2b68aa9cd85b98be9c2bc985d7343e5396974
medtijabou <tijarim7@gmail.com>
2025-04-24 07:45:07 +0200

    Corrigé pour que les mois correspondent

--- src/helpers/Date/index.js
+++ src/helpers/Date/index.js
@@ -16,6 +16,4 @@ export const MONTHS = {
 // Corrigé pour que les mois correspondent bien (0-based)
 export const getMonth = (date) => MONTHS[date.getMonth() + 1];
 
-// Affichage complet : 22 avril 2025
-export const getFormattedDate = (date) =>
-  `${date.getDate()} ${getMonth(date)} ${date.getFullYear()}`;
+
