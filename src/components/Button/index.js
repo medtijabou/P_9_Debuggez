336bc4cdc84cd59a7687c861fe18c7674d5b4ce6
medtijabou <tijarim7@gmail.com>
2025-04-24 13:20:03 +0200

    btn

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -8,20 +8,46 @@ export const BUTTON_TYPES = {
 };
 
 const Button = ({ title, onClick, type, disabled, children }) => {
-  const isSubmitButton = type === BUTTON_TYPES.SUBMIT;
-
-  return (
-    <button
-      type={isSubmitButton ? "submit" : "button"}
-      disabled={disabled}
-      className="Button"
-      data-testid="button-test-id"
-      onClick={!isSubmitButton ? onClick : undefined} // onClick seulement pour les boutons non submit
-      title={title}
-    >
-      {children}
-    </button>
-  );
+  switch (type) {
+    case BUTTON_TYPES.DEFAULT:
+      return (
+        <button
+          type="button"
+          disabled={disabled}
+          className="Button"
+          data-testid="button-test-id"
+          onClick={onClick}
+          title={title}
+        >
+          {children}
+        </button>
+      );
+    case BUTTON_TYPES.SUBMIT:
+      return (
+        <input
+          disabled={disabled}
+          className="Button"
+          type="submit"
+          data-testid="button-test-id"
+          value={children}
+          onClick={onClick}
+          title={title}
+        />
+      );
+    default:
+      return (
+        <button
+          type="button"
+          disabled={disabled}
+          className="Button"
+          data-testid="button-test-id"
+          onClick={onClick}
+          title={title}
+        >
+          {children}
+        </button>
+      );
+  }
 };
 
 // eslint-disable-next-line react/no-typos
@@ -32,13 +58,12 @@ Button.propTypes = {
   disabled: PropTypes.bool,
   children: PropTypes.node,
 };
-
 Button.defaultProps = {
   disabled: false,
   onClick: () => null,
   type: BUTTON_TYPES.DEFAULT,
   title: "",
-  children: null,
-};
+  children: null
+}
 
 export default Button;
